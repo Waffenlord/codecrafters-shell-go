e9bd655339b0f8affc0cf9f8847017473b23f7ba
Waffenlord <swilliamd.sanchez@gmail.com>
2025-08-19 21:54:59 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,14 +11,17 @@ var _ = fmt.Fprint
 
 func main() {
 
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("error occurred while reading your command")
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("error occurred while reading your command")
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 
-	fmt.Println(command[:len(command) - 1] + ": command not found")
 }
